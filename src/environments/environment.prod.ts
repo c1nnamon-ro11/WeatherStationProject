7aaa67df3a3f930b8cc5d89a915832f5067dd86d
c1nnamon-ro11 <vladislaw.vlad2@gmail.com>
2022-10-09 23:49:40 +0300

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'weather-drone-monitoring',
+    appId: '1:821794374324:web:60f8fd3cb6929745280de1',
+    databaseURL: 'https://weather-drone-monitoring-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'weather-drone-monitoring.appspot.com',
+    apiKey: 'AIzaSyA2sAKNcFawtyso80ngEzzjPXaW6L5Lh9Y',
+    authDomain: 'weather-drone-monitoring.firebaseapp.com',
+    messagingSenderId: '821794374324',
+  },
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA2sAKNcFawtyso80ngEzzjPXaW6L5Lh9Y",
+    authDomain: "weather-drone-monitoring.firebaseapp.com",
+    databaseURL: "https://weather-drone-monitoring-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "weather-drone-monitoring",
+    storageBucket: "weather-drone-monitoring.appspot.com",
+    messagingSenderId: "821794374324",
+    appId: "1:821794374324:web:60f8fd3cb6929745280de1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'weather-drone-monitoring',
+    appId: '1:821794374324:web:60f8fd3cb6929745280de1',
+    databaseURL: 'https://weather-drone-monitoring-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'weather-drone-monitoring.appspot.com',
+    apiKey: 'AIzaSyA2sAKNcFawtyso80ngEzzjPXaW6L5Lh9Y',
+    authDomain: 'weather-drone-monitoring.firebaseapp.com',
+    messagingSenderId: '821794374324',
+  },
   production: false
 };
 
